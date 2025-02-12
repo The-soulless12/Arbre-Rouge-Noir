5383345da62115fc895a4bb27a1f6f35622ddbf6
The-soulless12 <aklirayane2@gmail.com>
2025-02-12 21:09:24 +0100

    Fonctions de rotation de noeud vers la gauche et vers la droite

--- biblio.c
+++ biblio.c
@@ -59,3 +59,42 @@ void liberer_arbre(ArbreRN *arbre) {
     free(arbre->nil);
     free(arbre);
 }
+
+void rotation_gauche(ArbreRN *arbre, Noeud *x) {
+    // Effectue la rotation gauche du noeud X
+    Noeud *y = x->droite; // Y devient le fils droit de X
+    x->droite = y->gauche; // Le sous-arbre gauche de Y devient le sous-arbre droit de X
+    if (y->gauche != arbre->nil) { // Si Y avait un sous-arbre gauche, mettre à jour son parent
+        y->gauche->parent = x;
+    }
+    y->parent = x->parent; // Y prend la place de X en tant que fils de son parent
+    if (x->parent == arbre->nil) { // Si X était la racine, Y devient la nouvelle racine
+        arbre->racine = y;
+    } else if (x == x->parent->gauche) { // Si X était un fils gauche, Y devient le fils gauche du parent de X
+        x->parent->gauche = y;
+    } else { // Sinon, Y devient le fils droit du parent de X
+        x->parent->droite = y;
+    }
+    y->gauche = x; // X devient le fils gauche de Y
+    x->parent = y; // Mettre à jour le parent de X
+}
+
+void rotation_droite(ArbreRN *arbre, Noeud *x) {
+    // Effectue la rotation droite du noeud X
+    Noeud *y = x->gauche; // Y devient le fils gauche de X
+    x->gauche = y->droite; // Le sous-arbre droit de Y devient le sous-arbre gauche de X
+    if (y->droite != arbre->nil) { // Si Y avait un sous-arbre droit, mettre à jour son parent
+        y->droite->parent = x;
+    }
+    y->parent = x->parent; // Y prend la place de X en tant que fils de son parent
+    if (x->parent == arbre->nil) { // Si X était la racine, Y devient la nouvelle racine
+        arbre->racine = y;
+    } else if (x == x->parent->droite) { // Si X était un fils droit, Y devient le fils droit du parent de X
+        x->parent->droite = y;
+    } else { // Sinon, Y devient le fils gauche du parent de X
+        x->parent->gauche = y;
+    }
+    y->droite = x; // X devient le fils droit de Y
+    x->parent = y; // Mettre à jour le parent de X
+}
+
--- biblio.h
+++ biblio.h
@@ -20,11 +20,20 @@ typedef struct {
     Noeud *nil; // Représente les feuilles NULL noires
 } ArbreRN;
 
+// Régles des arbres rouge noir
+// 1. Chaque nœud est soit rouge soit noir.
+// 2. La racine est toujours noire.
+// 3. Toutes les feuilles (nil) sont noires.
+// 4. Un nœud rouge a toujours des fils noirs.
+// 5. Tout chemin d’un nœud à ses feuilles contient le même nombre de nœuds noirs.
+
 // Prototypes de la machine abstraite
 ArbreRN *creer_arbre();
 Noeud *creer_noeud(ArbreRN *arbre, int valeur);
 void afficher_arbre(Noeud *racine, Noeud *nil, int niveau);
 void liberer_noeuds(Noeud *noeud, Noeud *nil);
 void liberer_arbre(ArbreRN *arbre);
+void rotation_gauche(ArbreRN *arbre, Noeud *x);
+void rotation_droite(ArbreRN *arbre, Noeud *x);
 
 #endif // BIBLIO_H_INCLUDED
